9b36780d6bde29e36657076bf50e7ac95eec7c4b
Juan Builes <j_builes@hotmail.com>
2025-04-23 08:33:58 -0500

    Primer Commit2

--- sumaBasica.py
+++ sumaBasica.py
@@ -2,3 +2,4 @@ num1=int (input("ingrese un el primer numero: "))
 num2=int (input("ingrese un el segundo numero: "))
 
 print ("El resultado es: "+ str(num1+num2))
+print ("El resultado es: "+ str(num1+num2))

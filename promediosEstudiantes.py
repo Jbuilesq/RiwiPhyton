acc898fa9fb62402057ece275158dffdbfbfbb5e
Juan Builes <j_builes@hotmail.com>
2025-05-06 13:15:18 -0500

    Listas

--- promediosEstudiantes.py
+++ promediosEstudiantes.py
@@ -24,22 +24,23 @@ def addStudent ():
         grade = gradeValidation ()
         grades.append(grade)
     students.append({"estudiante":student,"nota":grades})
+    return student,grades
     
 
-def gradesAverage ():
+def gradesAverage (nameStudent,gradesStudent):
     print("\n-------- Promedios estudiante ---------\n")
-    for studentInfo in len(students):
-        print(studentInfo)
-        nameStudent = studentInfo["estudiante"]
-        print(nameStudent)
-        gradesStudent = studentInfo["nota"]
-        print(gradesStudent)
-        if gradesStudent:
-            avgGrades = sum(gradesStudent) / len(gradesStudent)
-            print(f"El promedio de {nameStudent} es: {avgGrades:.2f}")
-        else:
-            print(f"{nameStudent} no tiene calificaciones.")
-        gradesComparison(avgGrades)
+    # for studentInfo in len(students):
+    #     print(studentInfo)
+    #     nameStudent = studentInfo["estudiante"]
+    #     print(nameStudent)
+    #     gradesStudent = studentInfo["nota"]
+    #     print(gradesStudent)
+    if gradesStudent:
+        avgGrades = sum(gradesStudent) / len(gradesStudent)
+        print(f"El promedio de {nameStudent} es: {avgGrades:.2f}")
+    else:
+        print(f"{nameStudent} no tiene calificaciones.")
+    gradesComparison(avgGrades)
 
 
 
@@ -51,11 +52,13 @@ def gradesComparison (avgGrade):
    
 
 while True:
-    addStudent ()
-    gradesAverage()
+    student, grades = addStudent ()
+    gradesAverage(student,grades)
     option=input("Desea ingresar mas estudiantes: 0 = SI - 1 = NO: ")
     if option != "0":
         break
 
 print(students)
 print(type(students))
+print(students[0]["nota"])
+print(students[0]["nota"][2])
